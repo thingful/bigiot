5f71cd8a6151c17e64aa9b4c099c7b5c8bd85f56
Sam Mulube <sam@thingful.net>
2017-10-20 10:39:22 +0100

    Add license boilerplate to all files.

--- bigiot_test.go
+++ bigiot_test.go
@@ -1,3 +1,17 @@
+// Copyright 2017 Thingful Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package bigiot_test
 
 import (
@@ -209,8 +223,6 @@ func TestOffering(t *testing.T) {
 			&http.Header{
 				"Authorization": []string{"Bearer 1234abcd"},
 			},
-		//).WithBody(
-		//	bytes.NewBufferString(`{"query":"query offering { offering(id: "offeringId") { id name } }`),
 		),
 	)
 
@@ -223,5 +235,5 @@ func TestOffering(t *testing.T) {
 	offering, err := provider.Offering("offeringID")
 	assert.Nil(t, err)
 	assert.NotNil(t, offering)
-	//assert.Equal(t, "providerId", offering.ID)
+	assert.Equal(t, "offeringID", offering.ID)
 }
