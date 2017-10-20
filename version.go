bcc9e3a683daf284f01a671592a9b33fe81ceccd
Sam Mulube <sam@thingful.net>
2017-10-20 10:05:20 +0100

    Update version string

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package bigiot
 const (
 	// Version string - currently manually updated by make task. We use this
 	// string in the user agent header we set by default in the client
-	Version = "a7a3b8d-dirty"
+	Version = "c62c33c"
 )
