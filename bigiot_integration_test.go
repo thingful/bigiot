612bc67d454427bbba14542579bbb38edc2f23a6
Sam Mulube <sam@thingful.net>
2018-02-08 21:17:10 +0000

    WIP - updating integration test

--- bigiot_integration_test.go
+++ bigiot_integration_test.go
@@ -17,8 +17,10 @@
 package bigiot_test
 
 import (
+	"context"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/thingful/bigiot"
@@ -34,7 +36,7 @@ func Getenv(t *testing.T, key string) string {
 	return ""
 }
 
-func TestIntegrationAuthenticate(t *testing.T) {
+func TestProviderIntegration(t *testing.T) {
 	providerID := Getenv(t, "PROVIDER_ID")
 	providerSecret := Getenv(t, "PROVIDER_SECRET")
 
@@ -43,4 +45,51 @@ func TestIntegrationAuthenticate(t *testing.T) {
 
 	err = provider.Authenticate()
 	assert.Nil(t, err)
+
+	expirationTime := time.Now().Add(60 * time.Minute)
+
+	offeringInput := &bigiot.OfferingDescription{
+		LocalID: "TestOffering",
+		Name:    "Test Offering",
+		OutputData: []bigiot.DataField{
+			{
+				Name:   "value",
+				RdfURI: "schema:random",
+			},
+		},
+		Endpoints: []bigiot.Endpoint{
+			{
+				URI:                 "https://example.com/random",
+				EndpointType:        bigiot.HTTPGet,
+				AccessInterfaceType: bigiot.BIGIoTLib,
+			},
+		},
+		License: bigiot.OpenDataLicense,
+		Price: bigiot.Price{
+			Money: bigiot.Money{
+				Amount:   0.001,
+				Currency: bigiot.EUR,
+			},
+			PricingModel: bigiot.PerAccess,
+		},
+		Extent: bigiot.Address{
+			City: "Berlin",
+		},
+		Activation: bigiot.Activation{
+			Status:         true,
+			Duration:       10 * time.Minute,
+			ExpirationTime: expirationTime,
+		},
+	}
+
+	_, err = provider.RegisterOffering(context.Background(), offeringInput)
+	assert.Nil(t, err)
+	//assert.NotNil(t, offering.ID)
+
+	//deleteOffering := &bigiot.DeleteOffering{
+	//	ID: offering.ID,
+	//}
+
+	//err = provider.DeleteOffering(context.Background(), deleteOffering)
+	//assert.Nil(t, err)
 }
