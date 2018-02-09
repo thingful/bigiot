0054b967d3154286cec5d1cdb2597c198bedd2ad
Sam Mulube <sam@thingful.net>
2018-02-09 01:36:28 +0000

    Get rid of previous error types
    Try using errors returned by API

--- errors.go
+++ errors.go
@@ -14,17 +14,13 @@
 
 package bigiot
 
-const (
-	// ErrUnexpectedResponse is an error returned when we receive an unexpected
-	// response from the BIG IoT API.
-	ErrUnexpectedResponse = Error("Unexpected HTTP response code")
-)
+// Error is used for unmarshalling error messages from the marketplace
+type Error struct {
+	Message string `json:"message"`
+}
 
-// Error is a type alias for string, allowing us to export const error values
-type Error string
-
-// Error is the implementation of the error interface, allowing us to use our
-// Error type as an error.
-func (e Error) Error() string {
-	return string(e)
+// ErrorResponse is used to unmarshal the response from the marketplace in the
+// event of an error.
+type ErrorResponse struct {
+	Errors []Error `json:"errors"`
 }
